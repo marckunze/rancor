69532cdefabe15b1fa3fa408db03e4ea0b73c077
Marc Kunze <mkunze@gmail.com>
2014-11-15 21:13:36 -0800

    Should be fix on DataMapper setup link

--- rancor.rb
+++ rancor.rb
@@ -1,14 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+
 # sets up a new database in this directory
 configure :development do
 	DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/rancor.db")
 end
 
 configure :production do
-	DataMapper.setup(:default,
-		ENV['HEROKU_POSTGRESQL_RED_URL'])
+	DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 end
 
 
@@ -34,7 +34,7 @@ end
 
 post '/' do
 
-end
+end2
 
 get '/new_user' do
 	erb :new_user
