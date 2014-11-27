e89fb76a1ebfaf29f082f754821547febd6c75a6
Mark Hammond <mhammond@mail.ccsf.edu>
2014-11-26 16:42:54 -0800

    Ballots are now being saved in user_workspace. It's very rough, and I need someone to double check my work.

--- user_workspace.rb
+++ user_workspace.rb
@@ -1,5 +1,3 @@
-# This file is meant to act as a workspace for converting the SQL statements
-# into the appropriate DataMapper classes
 require 'bundler'
 Bundler.require
 
@@ -12,93 +10,94 @@ configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 end
 
-# DROP TABLE IF EXISTS access_to;
-# DROP TABLE IF EXISTS choices;
-# DROP TABLE IF EXISTS rancor;
-# DROP TABLE IF EXISTS User;
-#
-# Handled by DataMapper.auto_migrate!
-#
-# PRAGMA foreign_keys = ON;
-
-# CREATE TABLE User
-class User
+class Account
   include DataMapper::Resource
   include BCrypt
 
-  # id INTEGER PRIMARY KEY,
   property :id,         Serial
-  # username TEXT NOT NULL UNIQUE,
   property :username,   Text, required: true, unique: true
-  # email VARCHAR(320) NOT NULL UNIQUE,
   property :email,      String, length: 320, required: true, unique: true
   property :password,   BCryptHash
-  # joined_at DATETIME
   property :joined_at,  DateTime, default: DateTime.now
 
   has n, :poll
+
+  def self.authenticate(user, password)
+    u = first(username: user)
+    u ||= first(email: user)
+    return nil if u.nil?  # user not found
+
+    u = nil unless u.password == password
+    return u
+  end
+
+  def self.exists?(user)
+    self.username_exists?(user) || self.email_exists?(user)
+  end
+
+  def self.username_exists?(username)
+    !first(username: username).nil?
+  end
+
+  def self.email_exists?(email)
+    !first(email:email).nil?
+  end
 end
 
 class Poll  # Rancor is the name of the sinatra class
   include DataMapper::Resource
 
-  # CREATE TABLE rancor
-  storage_names[:default] = "rancor"
-  # rid INTEGER PRIMARY KEY,
   property :rid,        Serial
-  # question TEXT NOT NULL,
   property :question,   Text
-  # open BOOLEAN DEFAULT 1,
   property :open,       Boolean, default: true
-  # closedate DATETIME,
   property :closedate,  DateTime
 
-  # oid INTEGER NOT NULL,
-  # FOREIGN KEY (oid) REFERENCES User(id)
-  belongs_to :user, required: false
-  has n, :choice, child_key: :rid
+  belongs_to :account, required: false
+  has n, :options
+  has n, :ballots
+  has n, :rankings, through: :options
 end
 
-# CREATE TABLE choices(
-class Choice
+class Option
   include DataMapper::Resource
 
-  # cid INTEGER, PRIMARY KEY(rid,cid)
   property :cid,    Integer, key: true, unique: false
-  # option TEXT NOT NULL,
   property :option, Text, required: true
-  # count INTEGER DEFAULT 0,
   property :count, Integer, default: 0
 
-  # rid INTEGER,
-  # FOREIGN KEY (rid) REFERENCES rancor(rid)
-  belongs_to :poll, child_key: :rid, key: true
-  # PRIMARY KEY(rid,cid)
+
+  belongs_to :poll, key: true
+  has n, :rankings
 end
 
-###### I don't think the following is necessary with DataMapper's belong_to
-###### and has n properties. Please correct me if I'm wrong.
-####### This is to see which user can access which poll.
-####### Which is necessary if you want people other than the owner to have access to the poll.
+class Ballot
+  include DataMapper::Resource
 
-# CREATE TABLE IF NOT EXISTS access_to(
-# rid INTEGER,
-# id INTEGER,
-# voted BOOLEAN DEFAULT 0,
-# choice INTEGER DEFAULT null,
-# FOREIGN KEY (rid) REFERENCES rancor(rid),
-# FOREIGN KEY (id) REFERENCES User(id),
-# FOREIGN KEY (rid,choice) REFERENCES choices(rid,cid)
-# );
+  property :rid,      Serial
+  property :voter, IPAddress
+
+  belongs_to :poll
+  has n, :rankings
+end
+
+class Ranking
+  include DataMapper::Resource
+
+  property :rank, Integer
+
+  belongs_to :ballot, key: true
+  belongs_to :option, key: true
+  has 1, :poll, through: :option
+end
 
 DataMapper.finalize
 DataMapper.auto_migrate!
 
 # INSERT INTO User (id,username,email) VALUES(1,'p1','abc@email.com');
-p1 = User.create(username: 'p1', email: 'abc@email.com')
+p1 = Account.create(username: 'p1', email: 'abc@email.com')
 
 # INSERT INTO User (id,username,email) VALUES(2,'student','cdad@email.com');
-student = User.create(username: 'student', email: 'cdad@email.com')
+student = Account.create(username: 'student', email: 'cdad@email.com')
 
 # INSERT INTO rancor (rid,oid,question) VALUES(1,1,'What is for dinner?');
 dinner = Poll.create(question: 'What is for dinner?')
@@ -111,21 +110,35 @@ student.poll << drinks
 student.save
 
 # INSERT INTO choices (rid,cid,option) VALUES(1,1,'steak');
-dinner.choice << Choice.create(cid: 1, option: 'steak')
-
+steak = Option.create(cid: 1, option: 'steak')
+dinner.options << steak
 # INSERT INTO choices (rid,cid,option) VALUES(1,2,'sushi');
-dinner.choice << Choice.create(cid: 2, option: 'sushi')
+sushi = Option.create(cid: 2, option: 'sushi')
+dinner.options << sushi
 dinner.save
 
 # INSERT INTO choices (rid,cid,option) VALUES(2,1,'yes');
-drinks.choice << Choice.create(cid: 1, option: 'yes')
-
+yes = Option.create(cid: 1, option: 'yes')
+drinks.options << yes
 #INSERT INTO choices (rid,cid,option) VALUES(2,2,'no');
-drinks.choice << Choice.create(cid: 2, option: 'no')
+no = Option.create(cid: 2, option: 'no')
+drinks.options << no
 drinks.save
 
-###### Ignored because there is no access_to table currently.
-# INSERT INTO access_to (rid,id) VALUES(1,1);
-# INSERT INTO access_to (rid,id) VALUES(1,2);
-# INSERT INTO access_to (rid,id) VALUES(2,1);
-# INSERT INTO access_to (rid,id) VALUES(2,2);
+ballot = Ballot.create(voter: "127.0.0.1")
+rank_yes = Ranking.create(rank: 1)
+rank_no = Ranking.create(rank: 2)
+
+ballot.rankings << rank_yes
+ballot.rankings << rank_no
+
+yes.rankings << rank_yes
+yes.count = 3 - rank_yes.rank
+no.rankings << rank_no
+no.count = 3 - rank_no.rank
+drinks.ballots << ballot
+
+yes.save
+no.save
+ballot.save
+drinks.save
