cae123f1c34e7de4233ff32d8348f94fb78360d1
Marc Kunze <mkunze@gmail.com>
2014-11-28 15:42:58 -0800

    changed email to heroku address

--- rancor.rb
+++ rancor.rb
@@ -96,7 +96,7 @@ class Rancor < Sinatra::Base
 
     Pony.mail({
       :to => params['email'],
-      :from => 'rancor app',
+      :from => 'app31653694@heroku.com',
       :subject => 'Rancor account created', 
       :via => :smtp, 
       :body => 'test', 
