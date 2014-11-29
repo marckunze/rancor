40051201922b98c018162adb9f6d20fbc5eaa472
Marc Kunze <mkunze@gmail.com>
2014-11-28 17:42:54 -0800

    changed from body to html_body, trying to fix template rendering

--- rancor.rb
+++ rancor.rb
@@ -110,7 +110,7 @@ class Rancor < Sinatra::Base
       :from => 'app31653694@heroku.com',
       :subject => 'Rancor account created', 
       :via => :smtp, 
-      :body => @email_body, 
+      :html_body => @email_body, 
       :via_options => {
         :address  => 'smtp.mandrillapp.com', 
         :user_name => ENV['MANDRILL_USERNAME'],
