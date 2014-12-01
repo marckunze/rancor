6a8844a0e2e223a11c6ad6ed6d1f8ba1e671e2b8
Natasha <nvh82@yahoo.com>
2014-11-30 19:31:38 -0800

    Added results.css, results.js, and updated results.erb/rancor.rb to display voter results

---
+++ public/js/results.js
@@ -0,0 +1,8 @@
+/*! SOURCE : http://w3lessons.info/2013/06/04/skill-bar-with-jquery-css3/ */
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},1000);
+	});
+});
