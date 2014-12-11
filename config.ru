814de9026701a5a9bf0634e6cf1ce9e805028d0b
Natasha <nvh82@yahoo.com>
2014-12-11 00:12:51 -0800

    Added ellipsis to options and hover text

--- config.ru
+++ config.ru
@@ -3,5 +3,5 @@ require 'bundler'
 
 Bundler.require
 
-require_relative 'lib/rancor'
+require File.expand_path("../lib/rancor", __FILE__)
 run Rancor
--- public/js/results.js
+++ public/js/results.js
@@ -1,8 +1,12 @@
-/*! SOURCE : http://w3lessons.info/2013/06/04/skill-bar-with-jquery-css3/ */
+/*! SOURCE  http://w3lessons.info/2013/06/04/skill-bar-with-jquery-css3/ */
 jQuery(document).ready(function(){
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
 			width:jQuery(this).attr('data-percent')
 		},1000);
+	});
+
+	$('[data-toggle="tooltip"]').tooltip({
+    'placement': 'top'
 	});
 });
