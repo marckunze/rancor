3662ec92392111f6c32c7187fc6a0e2985e15802
Marc Kunze <mkunze@gmail.com>
2014-12-14 18:04:15 -0800

    Moved closing time to email helper

--- lib/helpers/email_helpers.rb
+++ lib/helpers/email_helpers.rb
@@ -25,6 +25,7 @@ module EmailHelpers
   # Returns nothing
   def send_invites(poll)
     @poll = poll
+    @closing_ hours = TimeDifference.between(Time.now, @poll.closedate).in_hours.floor
     @poll.invites.each do |invite|
       next if (!@poll.owner.nil?) && invite.email == @poll.owner.email
       send_email('You have been invited to participate in a poll!',
